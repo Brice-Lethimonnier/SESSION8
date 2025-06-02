7d54be5b8c087990e5fc49bebf71bbe222b98612
Brice Lethimonnier <37172498+Brism44@users.noreply.github.com>
2025-06-02 21:42:47 +0200

    Update command_encrypt.sh

--- command_encrypt.sh
+++ command_encrypt.sh
@@ -27,7 +27,7 @@ PYTHON_SCRIPT="./SESSION8/bin/hybrid_encrypt.py"
 
 if [ -f "$PYTHON_SCRIPT" ]; then
     echo "Exécution du script Python : $PYTHON_SCRIPT"
-    python3 "$PYTHON_SCRIPT "
+    python3 "$PYTHON_SCRIPT ./message.txt ./.ssh/keyPublic.pem /.ssh/keyPrivateSign.der /.ssh/keyPublicSign.pem"
 else
     echo "Erreur : Le fichier $PYTHON_SCRIPT n'existe pas dans le répertoire courant."
     exit 1
